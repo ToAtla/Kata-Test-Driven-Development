b963e2feb3325753610f18c978c5f1954a557776
Þórður Atlason <thorduratl17@ru.is>
2018-10-13 14:13:04 +0000

    Refactored: sum of array moved to a function

--- calculator.js
+++ calculator.js
@@ -1,28 +1,20 @@
 //calculator.js
 
-function add (input) {
-	if(input === ""){
-		return 0;
-	}
-	numberStrings = input.split(',');
+function sumOfArray(numberStrings){
 	sum = 0;
 	for(var i = 0; i < numberStrings.length; i++){
 		sum += parseInt(numberStrings[i]);
 	}
 
 	return sum;
+}
 
-/*
-	else if (!input.includes(',')){
-		return parseInt(input);
-	}else{
-		numberStrings = input.split(',');
-		if(numberStrings.length === 2)
-			return parseInt(numberStrings[0]) + parseInt(numberStrings[1]);
-		else
-			return parseInt(numberStrings[0]) + parseInt(numberStrings[1]) + parseInt(numberStrings[2]);
+function add (input) {
+	if(input === ""){
+		return 0;
 	}
-	*/
+	numberStrings = input.split(',');
+	return sumOfArray(numberStrings);
 
 }
 
