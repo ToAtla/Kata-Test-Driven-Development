c70ba9d87e3fea424ca6fbd44233329a422ee0e5
Þórður Atlason <thorduratl17@ru.is>
2018-10-13 14:06:24 +0000

    Add now returns the sum of two numbers

--- calculator.js
+++ calculator.js
@@ -1,11 +1,15 @@
 //calculator.js
 
-function add (number) {
-	if(number === ""){
+function add (input) {
+	if(input === ""){
 		return 0;
+	}else if (!input.includes(',')){
+		return parseInt(input);
+	}else{
+		numberStrings = input.split(',');
+		return parseInt(numberStrings[0]) + parseInt(numberStrings[1]);
 	}
 
-	return parseInt(number);
 }
 
 module.exports = add;
--- calculator.test.js
+++ calculator.test.js
@@ -7,6 +7,11 @@ it("should return zero on an empty string", () => {
 	expect(add("")).toBe(0);
 });
 
-it("should the argument when input is one number", () => {
+it("should return the argument when input is one number", () => {
 	expect(add("3")).toBe(3);
+});
+
+
+it("should return the sum of the arguments when input is two numbers", () => {
+	expect(add("3,5")).toBe(8);
 });
